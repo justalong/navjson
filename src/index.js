118251850add1bb54a5f8f3e05adc30cc6187c74
liuliu <273667422@qq.com>
2025-04-01 09:28:53 +0800

    open

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ module.exports = {
         "v3": {
             "link":"2025/3/24",
             "desc": ["更新ppt网站"]
+        },
+        "v4": {
+            "link":"2025/4/01",
+            "desc": ["更新开源资讯网站"]
         }
     },
     "网盘资源": {
@@ -220,6 +224,40 @@ module.exports = {
     },
     ...wall,
     ...ppt,
+    "重要资讯": {
+        "hellogithub": {
+            "link":"https://hellogithub.com/",
+            "desc": ["github开源资讯"]
+        },
+        "oschina": {
+            "link":"https://www.oschina.net",
+            "desc": ["开源中国"]
+        },
+        "Hacker News": {
+            "link":"https://news.ycombinator.com",
+            "desc": ["技术新闻聚合平台，常有 GitHub 热门项目被推荐"]
+        },
+        "Product Hunt": {
+            "link":"https://www.producthunt.com",
+            "desc": ["新产品发布平台，许多开源工具和库会在此推广"]
+        },
+        "Dev.to": {
+            "link":"https://dev.to",
+            "desc": ["开发者社区，大量开源工具的使用教程和推荐"]
+        },
+        "GitHub Topics&explore": {
+            "link":"https://github.com/topics",
+            "desc": ['https://github.com/explore', "github官方主题"]
+        },
+        "GitHub Topics&explore": {
+            "link":"https://github.com/topics",
+            "desc": ['https://github.com/explore', 'https://github.com/trending', "github官方主题"]
+        },
+        "GitHub Topics&explore": {
+            "link":"https://github.blog",
+            "desc": ["github博客"]
+        }
+    },
     "总导航": {
         "导航": {
             "link": "https://github.com/justalong/navjson/blob/main/docs/res.md",
